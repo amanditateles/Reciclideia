777597bc624e3c9e2e5a684f4e1d507b32764b45
unknown <amandateles466@gmail.com>
2022-10-18 20:47:17 -0400

    redefinir-senha

--- app/src/main/java/com/example/reciclideia/RedefinirSenha.kt
+++ app/src/main/java/com/example/reciclideia/RedefinirSenha.kt
@@ -3,20 +3,64 @@ package com.example.reciclideia
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.util.Log
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 
 class RedefinirSenha : AppCompatActivity() {
+
+    private val TAG = "RedefinirSenha"
+    private var etEmail: EditText? = null
+    private var buttonConcluir: Button? = null
+
+    private var mAuth: FirebaseAuth? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_redefinir_senha)
 
-        val buttonConcluir = findViewById<Button>(R.id.buttonConcluir)
-        buttonConcluir.setOnClickListener{
-            openActivity()
+        inicialise()
+    }
+
+    private fun inicialise(){
+        etEmail = findViewById(R.id.etEmail) as EditText
+        buttonConcluir = findViewById(R.id.buttonConcluir) as Button
+
+        mAuth = FirebaseAuth.getInstance ()
+
+        buttonConcluir!!.setOnClickListener { sendPasswordEmail () }
+
+    }
+
+    private fun sendPasswordEmail () {
+        val email = etEmail?.text.toString()
+
+        if (!TextUtils.isEmpty(email)) {
+            mAuth!!
+                .sendPasswordResetEmail(email)
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        val message = "Email Enviado."
+                        Log.d(TAG, message)
+                        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+                        updateUI()
+                    } else {
+                        Log.w(TAG, task.exception!!.message.toString())
+                        Toast.makeText(this,
+                            "Nenhum usuário encontrado com esse e-mail.",
+                            Toast.LENGTH_SHORT).show()
+                    }
+                }
+        } else{
+            Toast.makeText (this, "Entre com um e-mail válido", Toast.LENGTH_SHORT).show()
         }
     }
-    private fun openActivity(){
-        val intent = Intent(this, TelaInicial::class.java)
+    private fun updateUI(){
+        val intent = Intent(this@RedefinirSenha, MainActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
         startActivity(intent)
     }
 }
